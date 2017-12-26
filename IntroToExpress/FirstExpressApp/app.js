2fec0daf2679418c6c65947286bb1c8922f52ef5
Frank <fsr32@cornell.edu>
2017-12-25 20:59:59 -0500

    Created first express app with localhost:3000

---
+++ IntroToExpress/FirstExpressApp/app.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var app = express();
+
+// "/" => "Hi there!"
+app.get("/", function(req, res){
+	res.send("Hi there!");
+});
+// "/bye" => "Goodbye!"
+app.get("/bye", function(req, res){
+	console.log("Someone sent a request to /dog");
+	res.send("Goodbye!");
+});
+// "/dog" => "MEOW!
+app.get("/dog", function(req, res){
+	res.send("MEOW!");
+})
+
+// Tell Express to listen for requests (start server)
+app.listen(3000, function(){
+	console.log("Example app listening on port 3000");
+});
+
