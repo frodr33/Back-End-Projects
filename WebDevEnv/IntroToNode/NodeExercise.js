d75e07687844466711090905ba6a4298a3c258ee
Frank <fsr32@cornell.edu>
2017-12-25 19:12:53 -0500

    Adding some practice Nodejs

---
+++ WebDevEnv/IntroToNode/NodeExercise.js
@@ -0,0 +1,13 @@
+// Node Exercise
+
+/* Given 2 inputs, a str and int, 
+	print the str to the console 
+	a total of int times
+*/
+function echo(str, num) {
+	for (var i = 1; i <= num; i++){
+		console.log(str);
+	}
+}
+echo("Echo!!!", 10);
+echo("Tater Tots", 3);
