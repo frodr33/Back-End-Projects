6e94b7b49bcb2202fda663c7e2947acd6e794d81
Frank <fsr32@cornell.edu>
2017-12-29 15:44:43 -0500

    Comppleted movie API project implementing form also

--- Projects/MovieSearchApp/app.js
+++ Projects/MovieSearchApp/app.js
@@ -2,13 +2,23 @@ var express = require("express");
 var app = express();
 var request = require("request");
 
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+	res.render("search");
+});
+
+
+
 app.get("/results", function(req, res){
-	request("http://www.omdbapi.com/?s=california&apikey=thewdb",
-		function(error, response, body){
- 		console.log('error:', error); // Print the error if one occurred
- 		console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
- 		var results = JSON.parse(body);
- 		res.send(results["Search"][0]["Title"]);
+	var query = req.query.search;
+	var url = "http://www.omdbapi.com/?s=" + query + "&apikey=thewdb";
+
+	request(url, function(error, response, body){
+ 		var data = JSON.parse(body);
+ 		//res.send(results["Search"][0]["Title"]);
+ 		res.render("results", {data: data});
 		});
 });
 
