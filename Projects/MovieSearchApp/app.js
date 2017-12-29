3ea16038697694b92a7ded68c5c9326b02f8d3d5
Frank <fsr32@cornell.edu>
2017-12-29 14:10:05 -0500

    Took Data from OMDB Api and searched through it

--- Projects/MovieSearchApp/app.js
+++ Projects/MovieSearchApp/app.js
@@ -3,12 +3,12 @@ var app = express();
 var request = require("request");
 
 app.get("/results", function(req, res){
-	res.send("Route Works");
 	request("http://www.omdbapi.com/?s=california&apikey=thewdb",
 		function(error, response, body){
  		console.log('error:', error); // Print the error if one occurred
  		console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
- 		console.log(body);
+ 		var results = JSON.parse(body);
+ 		res.send(results["Search"][0]["Title"]);
 		});
 });
 
@@ -17,4 +17,4 @@ app.get("/results", function(req, res){
 // Start Server
 app.listen(3000, function(){
 	console.log("Server has started");
-})
+});
